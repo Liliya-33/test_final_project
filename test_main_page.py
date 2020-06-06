aa36e1ce5eb8a4732f244e156c0841aba8fd773c
Liliya-33 <liliya.kadyrova.0106@gmail.com>
2020-06-06 19:45:20 +0500

    add test_guest_should_be_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/ "
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -14,7 +15,9 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-#
-# def go_to_login_page(browser):
-#     login_link = browser.find_element_by_css_selector("#login_link")
-#     login_link.click()
+
+def test_guest_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
