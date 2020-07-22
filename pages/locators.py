8eb244b792c39b3a99425508294f68bc47ec0234
Liliya-33 <liliya.kadyrova.0106@gmail.com>
2020-07-22 23:25:15 +0500

    add MESSAGE_BASKET_EMPTY and ITEMS_IN_BASKET

--- pages/locators.py
+++ pages/locators.py
@@ -16,9 +16,12 @@ class LoginPageLocators():
 
 
 class ProductPageLocators():
+    BASKET_BUTTON_ON_PAGE = (By.CLASS_NAME, "btn-group")
     BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
     PRICE_BOOK = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
     NAME_BOOK = (By.CSS_SELECTOR, ".product_main h1")
     PRICE_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(3) strong")
     NAME_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(1) strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)")
+    MESSAGE_BASKET_EMPTY = (By.CSS_SELECTOR, "#content_inner p")
+    ITEMS_IN_BASKET = (By.CLASS_NAME, ".basket-items")
