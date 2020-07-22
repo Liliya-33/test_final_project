2b4d25c2bca734ea796d5164e9821c8b07f834e9
Liliya-33 <liliya.kadyrova.0106@gmail.com>
2020-07-22 23:26:06 +0500

    create class BasketPage and add defs

---
+++ pages/basket_page.py
@@ -0,0 +1,11 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class BasketPage(BasePage):
+    def should_be_basket_empty(self):
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_EMPTY), "Basket is not empty."
+
+
+    def should_be_not_basket_empty(self):
+        assert self.is_element_present(*ProductPageLocators.ITEMS_IN_BASKET), "Basket is empty"
