698a568a4e38b89f2f277f5fb03dd59fa70b0024
Liliya-33 <liliya.kadyrova.0106@gmail.com>
2020-07-22 19:00:06 +0500

    add defs: is_not_element_present and is_disappear

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,17 @@
-from selenium.common.exceptions import NoSuchElementException
+import math
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+from .locators import ProductPageLocators
 
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        #self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -14,9 +19,48 @@ class BasePage():
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except (NoSuchElementException):
+        except NoSuchElementException:
             return False
         return True
 
     def should_be_login_link(self):
         assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
+    def should_click_to_basket(self):
+        basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket.click()
+        assert "basket" in self.browser.current_url, "Basket unavailable"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        print(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+
