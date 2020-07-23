ca8ea044720f49458bc810e406e996bd9ebb7cc1
Liliya-33 <liliya.kadyrova.0106@gmail.com>
2020-07-23 14:25:01 +0500

    add def register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,18 @@
-from selenium.webdriver.common.by import By
-from .locators import LoginPageLocators, MainPageLocators
-
 from .base_page import BasePage
+from .locators import LoginPageLocators, MainPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_string = self.browser.find_element(*LoginPageLocators.EMAIL_REGISTER)
+        email_string.send_keys(email)
+        password_1_string = self.browser.find_element(*LoginPageLocators.PASSWORD_1_REGISTER)
+        password_1_string.send_keys(password)
+        password_2_string = self.browser.find_element(*LoginPageLocators.PASSWORD_2_REGISTER)
+        password_2_string.send_keys(password)
+        button_register = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        button_register.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
