29592e14427e3cc070175085c4a6a745fc00c129
Liliya-33 <liliya.kadyrova.0106@gmail.com>
2020-07-23 15:05:34 +0500

    clear code

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,8 +4,7 @@ from .locators import ProductPageLocators
 
 class BasketPage(BasePage):
     def should_be_basket_empty(self):
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_EMPTY), "Basket is not empty."
-
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_EMPTY), "Basket is empty should be present."
 
     def should_be_not_basket_empty(self):
-        assert self.is_element_present(*ProductPageLocators.ITEMS_IN_BASKET), "Basket is empty"
+        assert self.is_not_element_present(*ProductPageLocators.ITEMS_IN_BASKET), "Here should be no items in the list "
